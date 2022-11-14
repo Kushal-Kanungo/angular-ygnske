381e70a0844bb79efe7047b8c8d8eaee8075515e
Kushal-Kanungo <115695117+Kushal-Kanungo@users.noreply.github.com>
2022-11-14 19:55:44 +0530

    Simple Foms Completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  curly = '{{item}}';
-  data = ['Bruce', 'Tony', 'Andrew', 'Ram'];
-  objectData = [
-    { name: 'Kushal', age: 22, email: 'kushal@com' },
-    { name: 'Ram', age: 26, email: 'ram@com' },
-    { name: 'Tony', age: 50, email: 'tony@com' },
-  ];
+  getUserValue(values) {
+    console.log(values);
+  }
 }
