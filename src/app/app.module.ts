467fa0216246bc3c08f10bc4f35d7482c66533b6
Kushal-Kanungo <115695117+Kushal-Kanungo@users.noreply.github.com>
2022-11-14 12:19:05 +0530

    Module Done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { UsersModule } from './users/users.module';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { UserListComponent } from './user-list/user-list.component';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, UsersModule],
   declarations: [AppComponent, HelloComponent, UserListComponent],
   bootstrap: [AppComponent],
 })
