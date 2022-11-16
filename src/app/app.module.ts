255f6f43097a18da3cb1f75f3a376c5f9c91599b
Kushal-Kanungo <115695117+Kushal-Kanungo@users.noreply.github.com>
2022-11-16 22:26:41 +0530

    Createad a custom directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { CustomStyleDirective } from './custom-style.directive';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule],
+  declarations: [AppComponent, HelloComponent, CustomStyleDirective],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
