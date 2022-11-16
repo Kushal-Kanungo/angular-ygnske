2fe5cb6e1e832011a9819dc37dfcc4c703ce7c64
Kushal-Kanungo <115695117+Kushal-Kanungo@users.noreply.github.com>
2022-11-16 15:40:24 +0530

    Routing Done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app.routing.module';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { UserComponent } from './user/user.component';
+import { AdminComponent } from './admin/admin.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule, AppRoutingModule],
+  declarations: [AppComponent, HelloComponent, UserComponent, AdminComponent],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
