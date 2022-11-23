37b7a6645d4b619a0e777e508945c9e7cb3e66cd
Kushal-Kanungo <115695117+Kushal-Kanungo@users.noreply.github.com>
2022-11-23 14:51:12 +0530

    Lazy Loading

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  topicName = 'Topic Name';
-  desc = 'it is desc';
+  topicName = 'Lazy Loading';
+  desc = 'Only Load routes when required';
 }
