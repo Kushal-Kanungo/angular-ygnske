ed0916e0ec4ec7aa2eae589a3b1fe187cf2031f7
Kushal-Kanungo <115695117+Kushal-Kanungo@users.noreply.github.com>
2022-11-28 00:15:36 +0530

    Form Submit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 
+enum Priority {
+  HIGH = 'high',
+  MEDIUM = 'medium',
+  LOW = 'low',
+}
+
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
@@ -8,4 +14,10 @@ import { Component } from '@angular/core';
 export class AppComponent {
   topicName = 'Topic Name';
   desc = 'it is desc';
+
+  onSubmit(data) {
+    console.warn(data);
+    console.log(data.priority === Priority.HIGH);
+    console.log(Priority.HIGH);
+  }
 }
