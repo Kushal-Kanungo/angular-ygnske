385cbb334f0565f8afacca73f78269dcf3c99a27
Kushal-Kanungo <115695117+Kushal-Kanungo@users.noreply.github.com>
2022-11-28 15:21:30 +0530

    Reactive Form With Validations Done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validator, Validators } from '@angular/forms';
 
 @Component({
   selector: 'my-app',
@@ -8,4 +9,24 @@ import { Component } from '@angular/core';
 export class AppComponent {
   topicName = 'Topic Name';
   desc = 'it is desc';
+
+  loginForm = new FormGroup({
+    user: new FormControl('', [Validators.required, Validators.minLength(5)]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(8),
+    ]),
+  });
+
+  loginUser() {
+    console.log(this.loginForm.value);
+  }
+
+  get user() {
+    return this.loginForm.get('user');
+  }
+
+  get password() {
+    return this.loginForm.get('password');
+  }
 }
