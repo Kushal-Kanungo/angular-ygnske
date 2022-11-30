5521b51b38398876067f1b23694cf91faa5310df
Kushal-Kanungo <115695117+Kushal-Kanungo@users.noreply.github.com>
2022-11-30 19:42:11 +0530

    Observable in Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  topicName = 'Topic Name';
-  desc = 'it is desc';
+export class AppComponent implements OnInit {
+  topicName = 'Observable ';
+  desc =
+    'Instead of promise which provides all data at once observable provide data in stream';
+
+  apiData: string[] = ['Default Data'];
+
+  /* Observable provides data only when a observer is present */
+  myObservable = new Observable((observer) => {
+    console.warn('Observable is starting to emit data');
+
+    setTimeout(() => {
+      observer.next('Data 1');
+    }, 1000);
+
+    setTimeout(() => {
+      observer.next('Data 2');
+    }, 1000);
+    setTimeout(() => {
+      observer.next('Data 3');
+    }, 2000);
+    setTimeout(() => {
+      observer.next('Data 4');
+    }, 3000);
+    setTimeout(() => {
+      observer.next('Data 5');
+    }, 4000);
+    setTimeout(() => {
+      observer.next('Data 6');
+    }, 5000);
+    setTimeout(() => {
+      observer.next('Data 7');
+    }, 6000);
+
+    // observer.next('Data 1');
+    // observer.next('Data 2');
+    // observer.next('Data 3');
+    // observer.next('Data 4');
+    // observer.next('Data 5');
+    // observer.next('Data 6');
+    // observer.next('Data 7');
+  });
+
+  ngOnInit() {
+    this.myObservable.subscribe((emittedValue: string) => {
+      this.apiData.push(emittedValue);
+    });
+  }
 }
