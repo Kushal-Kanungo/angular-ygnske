81d27b823f995ee55f360d6075536a329d65184f
Kushal-Kanungo <115695117+Kushal-Kanungo@users.noreply.github.com>
2022-12-03 13:43:08 +0530

    2 files modified

--- src/app/custom-style.directive.ts
+++ src/app/custom-style.directive.ts
@@ -1,10 +1,11 @@
 import { Directive, ElementRef } from '@angular/core';
 
 @Directive({
-  selector: '[appCustomStyle]',
+  selector: '[orange]',
 })
 export class CustomStyleDirective {
   constructor(private el: ElementRef) {
-    el.nativeElement.style.color = 'red';
+    // el.nativeElement.style.color = 'red';
+    el.nativeElement.classList.add('dynamic');
   }
 }
